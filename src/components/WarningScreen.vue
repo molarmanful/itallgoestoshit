5f53e0043b0686a011881fd5390f18ddc2eeef83
molarmanful <benjinpang@gmail.com>
2023-02-25 14:17:09 -0500

    hmmm

--- src/components/WarningScreen.vue
+++ src/components/WarningScreen.vue
@@ -7,8 +7,8 @@ let fade = ref(true)
 
 <template>
   <TitleScreen :opacity="fade * 100" :pointer-events="fade ? 'auto' : 'none'" @dblclick="fade = false">
-    <h1 text-center>WARNING</h1>
-    <p>Contains flashing lights and distorted sounds.</p>
+    <h1 text-center>warning</h1>
+    <p>contains flashing lights and distorted sounds.</p>
     <p text-center animate-pulse>[double-click to continue]</p>
   </TitleScreen>
 </template>
